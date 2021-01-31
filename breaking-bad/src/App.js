4d05866298ed365b1d77743dcc4649f1c7fc50c9
Akash kumar <akaysharma894@gmail.com>
2021-01-31 18:21:25 +0530

    updated

--- breaking-bad/src/App.js
+++ breaking-bad/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/ui/Header';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      Hello world!
+    <div className="container">
+      <Header />
     </div>
   );
 }
